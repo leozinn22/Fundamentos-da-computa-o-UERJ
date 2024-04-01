3fd1a8b41b2ac77c201ecf4150a30e1b3541fd08
leonardo <leozinhodosal222@gmail.com>
2024-04-01 17:14:57 -0300

    fix

--- Lista 1/1/init.py
+++ Lista 1/1/init.py
@@ -1,31 +1,45 @@
-import random
-#adicionando 10 dados ao array
-somaSalarioH = 0
-somaSalarioF = 0
-quantidadeH = 0
-quantidadeM=0
-maiorSalarioMenor30 = 0
-for i in range(10):
-    idade = int(random.randint(18,65))
-    sexo = str(random.choice(["H","M"]))
-    salario = float(random.uniform(1400,1000))
+homem = 'M'
+mulher = 'F'
+mediaF = 0 
+mediaM = 0
+somaF = 0
+somaM = 0
+MS = 0
+nF = 0 
+nM = 0 
+continua = 'S'
+while continua == 'S': 
+    sexo = input('Informe o sexo(M/F): ')
+    a = sexo.upper()
 
-    if sexo == "H":
-        somaSalarioH += salario
-        quantidadeH += 1
-    elif sexo == "M":
-        somaSalarioH += salario
-        quantidadeM += 1
-    if idade < 30 and salario > maiorSalarioMenor30: maiorSalarioMenor30 = salario 
-if quantidadeH <= 0:
-    print(str("A quantidade de homens é 0, não é possível calcular a média com esse valor!"))
-else:
-    mediaSalarioH = somaSalarioH/quantidadeH
-    print(f"Média salarial dos homens {mediaSalarioH:.2f}") 
+    while ((a != homem) and (a != mulher)):
+        a = input('Informação inválida! Insira M ou F: ')
+        a = a.upper()
 
-if quantidadeM <= 0:
-    print("A quantidade de mulheres é 0, não é possível calcular a média com esse valor!")
-else:
-    mediaSalarioM = somaSalarioH/quantidadeM
-    print(f"Média salarial das mulheres {mediaSalarioM:.2f} ")
-print(f"maior salario encontrado entre as pessoas abaxio de 30 anos {maiorSalarioMenor30:.2f}" )
+    idade = int(input('Insira a respectiva idade: '))
+    while idade < 18 or idade > 65:
+        idade = int(input('Idade inválida! Digite novamente: '))
+    salario = float(input('Insira o salário recebido: R$'))
+    while salario <= 0: 
+        salario = int(input('Salário inválido! Insira um salário válido: R$'))
+   
+    if a == mulher:
+        nF += 1
+        somaF += salario
+    elif a == homem: 
+        nM += 1
+        somaM += salario
+    if(idade <= 30 and salario >MS):
+        MS=salario
+    b = input('Deseja inserir mais informações? Digite "S" para sim e "N" para não. Caso deseje parar, a média salarial será calculada e faremos uma análise salarial entre as pessoas abaixo de 30 anos: ')
+    continua = b.upper()
+if nF > 0:
+    mediaF = (mediaF+somaF)/nF
+if nM > 0:
+    mediaM = (somaM+mediaM)/nM
+if nF == 0: 
+    mediaF = 0 
+if nM == 0:
+    mediaM = 0
+print('A média salarial dos homens e das mulheres é, respectivamente, R${:.2f} e R${:.2f}.'.format(mediaM, mediaF))
+print('O maior salário encontrado entre as pessoas abaixo de 30 anos foi R${:.2f}.'.format(MS))
